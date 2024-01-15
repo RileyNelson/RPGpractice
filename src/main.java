11c59e6772623800f045a5accc3ab511aa96b3e6
Riley Nelson <rileyln1992@gmail.com>
2024-01-14 17:33:29 -0700

    Initial commit with main

---
+++ src/main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class main{
+
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        System.out.println("Welcome! to start choose which class you would like to be?");
+        while(!scnr.nextLine().equals("exit")){
+            System.out.println( "Congrats you still playing......or are you?????");
+        }
+    }
+}
